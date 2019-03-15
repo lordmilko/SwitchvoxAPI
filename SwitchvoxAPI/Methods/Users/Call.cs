2f67da1adfa296c6dfe54e60119f148a6784eb7a
Kirill <spder.bkv@gmail.com>
2019-03-15 08:48:25 -0400

    Added methods "users.call" and "users.getMyInfo".

---
+++ SwitchvoxAPI/Methods/Users/Call.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SwitchvoxAPI.Methods
+{
+    /// <summary>
+    /// Generate a call between two numbers using the phone system.
+    /// </summary>
+    public partial class Users
+    {
+        /// <summary>
+        /// Generate a call between two numbers using the phone system.
+        /// </summary>
+        /// <param name="dialFirst">The number to ring first; this number will then appear to "dial" the second number</param>
+        /// <param name="dialSecond">The second number to ring. When the call to the first number is answered, the PBX will dial the second number (then connect both parties together)</param>
+        /// <param name="accountId">The Account ID of the account whose Call Rules and API settings should be used to place the calls. When dialFirst is an extension, this is typically that extension's Account ID.</param>
+        /// <param name="ignoreUserApiSettings">Whether the Call API Settings (digits to prepend, etc) of accountId should be ignored</param>
+        /// <param name="callerIdName">The Caller ID to display on the phone called by dialFirst.</param>
+        /// <param name="ignoreUserCallRules">Whether the call rules defined under the extension belonging to accountId should be ignored when generating the call.</param>
+        /// <param name="timeout">How long, in seconds, the phone system should attempt to ring dialFirst without being answered before giving up</param>
+        /// <param name="variables">A dictionary of variables to update in your IVR. The key is the variable name and the value is the value to set it to.</param>
+        public void Call(string dialFirst, string dialSecond, string accountId = null, bool ignoreUserApiSettings = false, string callerIdName = "PBX", bool ignoreUserCallRules = false, int timeout = 30, Dictionary<string, string> variables = null)
+        {
+            if (accountId == null || accountId == "")
+            {
+                ExtensionInfo extInfo = GetMyInfo();
+
+                if (extInfo == null || extInfo.AccountId == "")
+                    throw new ArgumentException("Account ID must be specified.");
+
+                accountId = extInfo.AccountId;
+            }
+
+            List<XElement> vars = new List<XElement>
+            {
+                new XElement("dial_first", dialFirst),
+                new XElement("dial_second", dialSecond),
+                new XElement("account_id", accountId),
+                new XElement("caller_id_name", callerIdName),
+                new XElement("timeout", timeout),
+                new XElement("ignore_user_api_settings", Convert.ToInt32(ignoreUserApiSettings)),
+                new XElement("ignore_user_call_rules", Convert.ToInt32(ignoreUserCallRules))
+            };
+
+            if (variables != null)
+            {
+                List<XElement> variableList = variables.Select(var => new XElement("variable", var.Key + " = " + var.Value)).ToList();
+
+                vars.Add(new XElement("variables", variableList));
+            }
+
+            var response = client.Execute(new RequestMethod("switchvox.users.call", vars));
+        }
+    }
+}
---
+++ SwitchvoxAPI/Methods/Users/GetMyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SwitchvoxAPI.Methods
+{
+    public partial class Users
+    {
+        private string tagGroup;
+        private string tagInstance;
+
+        /// <summary>
+        /// Fetch basic information about the extensions on a phone system.
+        /// </summary>
+        /// <param name="identifier">A <see cref="ExtensionIdentifier"/> value indicating whether Extension numbers or Account IDs will be used to get info for the extensions on your system</param>
+        /// <param name="values">A list of Extension Account IDs or Extension Numbers to get information for.</param>
+        /// <returns></returns>
+        public ExtensionInfo GetMyInfo()
+        {
+            var extension = client.Execute<ExtensionInfo>(new RequestMethod("switchvox.users.getMyInfo", null));
+
+            return extension;
+        }
+    }
+}
